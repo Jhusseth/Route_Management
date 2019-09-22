bcd1e915fdd9a22e758a1e9816e968c54e027d71
jean0206 <jeanortiz00@gmail.com>
2019-09-21 22:02:33 -0500

    Se porganizó la base de datos, se creó el mapa, y se añadieron solo las estaciones

---
+++ Route__Managment/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Route__Managment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.zoomBar = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.zoomBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(12, 12);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(545, 470);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(573, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 37);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ver estaciones";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // zoomBar
+            // 
+            this.zoomBar.Location = new System.Drawing.Point(654, 36);
+            this.zoomBar.Maximum = 25;
+            this.zoomBar.Name = "zoomBar";
+            this.zoomBar.Size = new System.Drawing.Size(196, 45);
+            this.zoomBar.TabIndex = 2;
+            this.zoomBar.Scroll += new System.EventHandler(this.ZoomBar_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(599, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Zoom";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 494);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.zoomBar);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.gMapControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.zoomBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar zoomBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Route__Managment/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using Modelo;
+
+namespace Route__Managment
+{
+    public partial class Form1 : Form
+    {
+        
+        GMarkerGoogle marker;
+        GMapOverlay markerOverlay;
+        DataTable dt;
+
+        int rowSelected = 0;
+        double Initiallatitude = 3.42158;
+        double initialLength = -76.5205;
+        private MetroCali MetroCa;
+        public Form1()
+        {
+            InitializeComponent();
+            MetroCa = new MetroCali();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {   
+            //Esta parte se usó para crear elarchivo.dat que da la parsistencia 
+            //a la HashTable que uso en la clase MetroCali.
+            /*
+            int counter = 0;
+            String line;
+            StreamReader file = new StreamReader("stations1.csv");
+            while ((line = file.ReadLine()) != null)
+            {
+                String[] separator = line.Split(';');
+                String busId = separator[0];
+                String longName = separator[3];
+                double latitude = Convert.ToDouble(separator[7]);
+                double lenght = Convert.ToDouble(separator[6]);
+                BusStation b = new BusStation(busId, longName, latitude, lenght);
+                MetroCa.serialize(b);
+            }
+            MessageBox.Show("Finalizó");*/
+            MetroCa.deserialize();
+            //BusStation b =(BusStation) MetroCa.BusStations["512055"];
+            //MessageBox.Show(""+ b.LongName);
+            paintStations();
+        }
+
+        public void paintStations()
+        {
+            MetroCa.deserialize();
+            foreach (String de in MetroCa.BusStations.Keys)
+            {
+                BusStation b = (BusStation)MetroCa.BusStations[de];
+                //MessageBox.Show(b.Latitude1 + " " + b.Lenght);
+                showStations(b.Latitude1, b.Lenght, b.LongName);
+
+            }
+
+            gMapControl1.Zoom = 12;
+            
+
+        }
+
+        public void showStations(double lat, double len, String name)
+        {
+            PointLatLng point = new PointLatLng(lat, len);
+            marker = new GMarkerGoogle(point, GMarkerGoogleType.blue_dot);
+            markerOverlay = new GMapOverlay("markers");
+            markerOverlay.Markers.Add(marker);
+            marker.ToolTipMode = MarkerTooltipMode.OnMouseOver;
+            marker.ToolTipText = string.Format(name);
+            gMapControl1.Overlays.Add(markerOverlay);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gMapControl1.DragButton = MouseButtons.Left;
+            gMapControl1.CanDragMap = true;
+            gMapControl1.MapProvider = GMapProviders.GoogleMap;
+            gMapControl1.Position = new PointLatLng(Initiallatitude, initialLength);
+            gMapControl1.MinZoom = 1;
+            gMapControl1.MaxZoom = 100;
+            gMapControl1.Zoom = 11;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            zoomBar.Value = Convert.ToInt32(gMapControl1.Zoom);
+        }
+
+        private void ZoomBar_Scroll(object sender, EventArgs e)
+        {
+            gMapControl1.Zoom = zoomBar.Value;
+        }
+    }
+}
