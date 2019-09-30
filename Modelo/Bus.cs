c428e17ddca903f5b5d979614a8d5877f2927ee5
Jhusseth <jhusseth.sanchez@correo.icesi.edu.co>
2019-09-30 15:58:23 -0500

    was implemeneted the classs bus and thread for the move

---
+++ Modelo/Bus.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelo
+{
+    class Bus
+    {
+
+        private int idBus;
+        private int tripId;
+        private List<Double> lenght;
+        private List<Double> latitude;
+   
+        public Bus(int idBus, int tripId, List<Double> lenght, List<Double> latitude)
+        {
+            this.IdBus = idBus;
+            this.TripId = tripId;
+            this.Lenght = lenght;
+            this.Latitude = latitude;
+
+        }
+
+
+
+
+        public void addLenght(double l)
+        {
+            lenght.Add(l);
+        }
+
+        public void addLatitude(double l)
+        {
+            lenght.Add(l);
+        }
+
+
+        public int IdBus { get => idBus; set => idBus = value; }
+        public int TripId { get => tripId; set => tripId = value; }
+        public List<Double> Lenght { get => lenght; set => lenght = value; }
+        public List<Double> Latitude { get => latitude; set => latitude = value; }
+    }
+}
