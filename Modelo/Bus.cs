ff5c40ddeceebffebe79542ac64eb408a1b5c900
Jhusseth <jhusseth.sanchez@correo.icesi.edu.co>
2019-11-11 00:30:40 -0500

    insert new projects and final stage

---
+++ Modelo/Bus.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelo
+{
+	public class Bus
+	{
+
+		public Bus(string date, string hour, int stopId, double latitude, double longitude, int taskId, int lineId, int tripId, long dataGramId,int busId)
+		{
+			this.date = date;
+			this.hour = hour;
+			this.stopId = stopId;
+			this.latitude = latitude;
+			this.longitude = longitude;
+			this.taskId = taskId;
+			this.lineId = lineId;
+			this.tripId = tripId;
+			this.dataGramId = dataGramId;
+			this.busId = busId;
+		}
+
+		public string date { get; set; }
+		public string hour { get; set; }
+		public int stopId { get; set; }
+		public double latitude { get; set; }
+		public double longitude { get; set; }
+		public int taskId { get; set; }
+		public int lineId { get; set; }
+		public int tripId { get; set; }
+		public int busId { get; set; }
+		public long dataGramId { get; set; }
+	}
+}
---
+++ Modelo/MetroCali.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Modelo
+{
+
+    public class MetroCali
+    {
+        private Hashtable busStations;
+        private Hashtable busStops;
+        private Hashtable lines;
+
+		public List<Zone> zonas { get; set; }
+
+
+		public Hashtable BusStations { get => busStations; set => busStations = value; }
+
+        public Hashtable BusStops { get => busStops; set => busStops = value; }
+        
+        public Hashtable Lines { get => lines; set => lines = value; }
+
+        public MetroCali()
+        {
+            BusStations = new Hashtable();
+            BusStops = new Hashtable();
+            Lines = new Hashtable();
+			this.zonas = new List<Zone>();
+			loadZones();
+		}
+
+        public void deserializeS()
+        {
+            FileStream fs = new FileStream("DataFile.dat", FileMode.Open);
+            try
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                BusStations = (Hashtable)formatter.Deserialize(fs);
+            }
+            catch (SerializationException e)
+            {
+                Console.WriteLine("Failed to deserialize. Reason: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+        public void deserializeP()
+        {
+            FileStream fs = new FileStream("DataFileP.dat", FileMode.Open);
+            try
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                BusStops = (Hashtable)formatter.Deserialize(fs);
+            }
+            catch (SerializationException e)
+            {
+                Console.WriteLine("Failed to deserialize. Reason: " + e.Message);
+                throw;
+            }
+            finally
+            {
+                fs.Close();
+            }
+        }
+
+  
+        public void dataReadingLines(String file)
+        {
+            StreamReader st = new StreamReader(file);
+            String lin = "";
+            st.ReadLine();            
+            while ((lin = st.ReadLine()) != null)
+            {
+                String[] line = lin.Split(';');
+                int idLine = Convert.ToInt32(line[0]);
+                string shortName = line[2];
+                Line l = new Line(idLine,shortName, line[3]);
+                addLines(l,l.LineId);
+            }
+        }
+
+        public void dataSerealize()
+        {
+            dataReadingLines("lines.csv");
+        }
+
+		public void loadZones()
+		{
+			Zone z0 = new Zone(3.457664, 3.441622, -76.598407, -76.524088);
+			zonas.Add(z0);
+
+			Zone z1 = new Zone(3.380145, 3.287627, -76.598407, -76.449366);
+			zonas.Add(z1);
+
+			Zone z2 = new Zone(3.516009, 3.457664, -76.598407, -76.505034);
+			zonas.Add(z2);
+
+			Zone z3 = new Zone(3.516009, 3.449274, -76.505034, 76.449366);
+			zonas.Add(z3);
+
+			Zone z4 = new Zone(3.449274, 3.442681, -76.524088, -76.460200);
+			zonas.Add(z4);
+
+			Zone z5 = new Zone(3.442681, 3.380145, -76.524088, -76.460200);
+			zonas.Add(z5);
+
+			Zone z6 = new Zone(3.441622, 3.380145, -76.598407, -76.524088);
+			zonas.Add(z6);
+
+			Zone z7 = new Zone(3.441622, 3.380145, -76.598407, -76.524088);
+			zonas.Add(z7);
+		}
+
+		public int isZone(double altitud, double longitud)
+		{
+			int pos = -1;
+			for (int i = 0; i < zonas.Count; i++)
+			{
+				Zone z = zonas[i];
+				if ( altitud >= z.LatitudeMin && altitud <= z.LatitudeMax)
+				{
+					if (longitud <= z.LongitudeMin && longitud >= z.LongitudeMax)
+					{
+						pos = i;
+					}
+				}
+			}
+			return pos;
+		}
+
+		public void addLines(Line line, int lineId) {
+            Lines.Add(lineId,line);
+        }
+    }  
+
+
+}
