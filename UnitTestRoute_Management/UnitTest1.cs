2cc55d7bed42f8044d746fa4eb748ddfa6fbaceb
santiago alejandro rosero cordoba <santimom1@hotmail.com>
2019-11-14 10:41:42 -0500

    create first unitary proof about MetroCali class

--- UnitTestRoute_Management/UnitTest1.cs
+++ UnitTestRoute_Management/UnitTest1.cs
@@ -1,4 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Modelo;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
 
 namespace UnitTestRoute_Management
 {
@@ -7,10 +11,16 @@ namespace UnitTestRoute_Management
     [TestClass]
     public class UnitTest1
     {
+    
 
         [TestMethod]
-        public void TestMethod1()
+        public void TestIsZone()
         {
+            MetroCali mc = new MetroCali();
+            //mc.isZone(3.30, 75.78);
+            int exceptedZone = 1;
+            Assert.AreEqual(exceptedZone, mc.isZone(3.287627, -76.449366));
         }
+
     }
 }
